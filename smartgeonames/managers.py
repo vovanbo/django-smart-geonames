28729690a79b0e0b690b9b1058044f0d95b6a502
Vladimir Bolshakov <vovanbo@gmail.com>
2015-12-25 02:20:35 +0300

    Fix import filter values in managers.

--- smartgeonames/managers.py
+++ smartgeonames/managers.py
@@ -2,35 +2,35 @@
 from __future__ import unicode_literals, absolute_import
 from django.db import models
 
-from .settings import (OBJECTS_CONTINENTS_FILTER,
-                       OBJECTS_COUNTRIES_FILTER,
-                       OBJECTS_CITIES_FILTER,
-                       OBJECTS_REGIONS_FILTER)
+from .settings import (OBJECTS_CONTINENTS_FILTER_VALUES,
+                       OBJECTS_COUNTRIES_FILTER_VALUES,
+                       OBJECTS_CITIES_FILTER_VALUES,
+                       OBJECTS_REGIONS_FILTER_VALUES)
 
 
 class ContinentManager(models.Manager):
     def get_queryset(self):
         return super(ContinentManager, self).get_queryset().filter(
-            featrue_code__in=OBJECTS_CONTINENTS_FILTER
+            featrue_code__in=OBJECTS_CONTINENTS_FILTER_VALUES
         )
 
 
 class CountryManager(models.Manager):
     def get_queryset(self):
         return super(CountryManager, self).get_queryset().filter(
-            feature_code__in=OBJECTS_COUNTRIES_FILTER
+            feature_code__in=OBJECTS_COUNTRIES_FILTER_VALUES
         )
 
 
 class CityManager(models.Manager):
     def get_queryset(self):
         return super(CityManager, self).get_queryset().filter(
-            feature_code__in=OBJECTS_CITIES_FILTER
+            feature_code__in=OBJECTS_CITIES_FILTER_VALUES
         )
 
 
 class RegionManager(models.Manager):
     def get_queryset(self):
         return super(RegionManager, self).get_queryset().filter(
-            feature_code__in=OBJECTS_REGIONS_FILTER
+            feature_code__in=OBJECTS_REGIONS_FILTER_VALUES
         )
